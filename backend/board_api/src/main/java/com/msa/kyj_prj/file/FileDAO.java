87a756b2dc5510391a24d7e8e9e80f16cac25f64
yoo07134 <yoo07134@naver.com>
2025-06-20 14:22:12 +0900

    파일 수정 중 삭제 기능 추가

--- backend/board_api/src/main/java/com/msa/kyj_prj/file/FileDAO.java
+++ backend/board_api/src/main/java/com/msa/kyj_prj/file/FileDAO.java
@@ -8,6 +8,6 @@ import org.apache.ibatis.annotations.Mapper;
 public interface FileDAO {
     void insertFile(FileEntity file); // 파일 삽입
     List<FileEntity> findFilesByBoardId(Long board_id); // 파일 리스트 검색
-    void deleteFilesByBoardId(Long board_id); // 게시글 삭제 시 같이 처리
+    void deleteFilesByBoardId(Long file_id); // 게시글 삭제 시 같이 처리
     public FileEntity findFileById(Long file_id); // 파일 상세정보 호출
 }
