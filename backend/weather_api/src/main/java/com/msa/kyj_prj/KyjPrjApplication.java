f0ac057dba8e8ed5733e67d983ab646d692f115c
yoo07134 <yoo07134@naver.com>
2025-06-21 09:41:32 +0900

    날씨 api 데이터 db에 저장 확인

--- backend/weather_api/src/main/java/com/msa/kyj_prj/KyjPrjApplication.java
+++ backend/weather_api/src/main/java/com/msa/kyj_prj/KyjPrjApplication.java
@@ -1,8 +1,10 @@
 package com.msa.kyj_prj;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.msa.kyj_prj")
 @SpringBootApplication
 public class KyjPrjApplication {
 
