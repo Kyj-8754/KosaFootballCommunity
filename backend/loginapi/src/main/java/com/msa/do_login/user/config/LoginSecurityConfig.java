433f64154f327c40e32854a58a5f1fb1f64f37ff
Do-seungjin <dosjin01@gmail.com>
2025-06-30 09:59:50 +0900

    [도승진] 권한 설정 변경

--- backend/loginapi/src/main/java/com/msa/do_login/user/config/LoginSecurityConfig.java
+++ backend/loginapi/src/main/java/com/msa/do_login/user/config/LoginSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.msa.do_login.user.config;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
