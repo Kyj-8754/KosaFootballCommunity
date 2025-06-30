d4d94195ed2b3bb38a4731e16d31a9b8b7ee8163
Do-seungjin <dosjin01@gmail.com>
2025-06-30 20:39:43 +0900

    [도승진] 마이페이지 구성중

--- backend/loginapi/src/main/java/com/msa/do_login/myPage/controller/MyPageController.java
+++ backend/loginapi/src/main/java/com/msa/do_login/myPage/controller/MyPageController.java
@@ -5,12 +5,12 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.msa.do_login.myPage.dto.UserInfoDTO;
 import com.msa.do_login.myPage.service.MyPageService;
+import com.msa.do_login.user.vo.UserVO;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,15 +23,14 @@ public class MyPageController {
 	private final MyPageService myPageService;
 
 	// 회원 상세정보 조회
-	@GetMapping("/detailView/{userNo}")
-	public ResponseEntity<Map<String, Object>> getMemberDetail(@PathVariable int userNo) {
-		log.info("회원 상세정보 조회 요청 도착");
-		UserInfoDTO memberInfo = myPageService.getMemberInfo(userNo);
-		if (memberInfo == null) {
+	@GetMapping("/detailView")
+	public ResponseEntity<Map<String, Object>> getMemberDetail(@RequestParam int userNo) {
+		UserVO userInfo = myPageService.getUserVO(userNo);
+		log.info("회원 정보 ="+userInfo);
+		if (userInfo == null) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND)
 					.body(Map.of("res_code", "404", "res_msg", "해당 사용자를 찾을 수 없습니다."));
 		}
-		log.info("회원 상세정보 조회 결과 반환 = {}",memberInfo);
-		return ResponseEntity.ok(Map.of("res_code", "200", "res_msg", "회원 정보 조회 성공", "member", memberInfo));
+		return ResponseEntity.ok(Map.of("res_code", "200", "res_msg", "회원 정보 조회 성공", "member", userInfo));
 	}
 }
