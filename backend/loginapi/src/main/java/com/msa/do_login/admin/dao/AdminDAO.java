1734d6f342a1244018ba1d17743a41c76de72167
Do-seungjin <dosjin01@gmail.com>
2025-07-04 16:01:01 +0900

    [도승진] 관리자 권한 사용자 목록 조회 구현

--- backend/loginapi/src/main/java/com/msa/do_login/admin/dao/AdminDAO.java
+++ backend/loginapi/src/main/java/com/msa/do_login/admin/dao/AdminDAO.java
@@ -3,10 +3,17 @@ package com.msa.do_login.admin.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.msa.do_login.user.vo.UserVO;
 
 @Mapper
 public interface AdminDAO {
-	public List<UserVO> getUserList();
+	
+	public List<UserVO> getUserList(@Param("limitPageNo") int limitPageNo,
+			  @Param("numPerPage")int numPerPage,
+			  @Param("searchType") String searchType,
+			  @Param("searchValue")String searchValue);
+
+	public int getTotalCount(@Param("searchType") String searchType, @Param("searchValue") String searchValue);
 }
