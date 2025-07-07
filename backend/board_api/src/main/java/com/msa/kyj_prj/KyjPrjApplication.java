d41afc73a3960d546fd8ff85a5d9a75b3b6c284c
Sibaru1217 <leehakbin1217@gmail.com>
2025-07-07 16:58:06 +0900

    웅희의 match의 웹소켓 구독을 시도 하려 했지만 결제 까지 연동이 안되서 중단함

--- backend/board_api/src/main/java/com/msa/kyj_prj/KyjPrjApplication.java
+++ backend/board_api/src/main/java/com/msa/kyj_prj/KyjPrjApplication.java
@@ -2,14 +2,15 @@ package com.msa.kyj_prj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.msa.kyj_prj") // 🔍 config 포함 모든 하위 패키지 스캔
 @EnableScheduling
 public class KyjPrjApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KyjPrjApplication.class, args);
 	}
-
 }
