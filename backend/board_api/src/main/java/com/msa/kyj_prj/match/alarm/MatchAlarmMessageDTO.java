c8f7b7a7a707fa8755000c223b6f10108207e574
Sibaru1217 <leehakbin1217@gmail.com>
2025-07-07 16:58:06 +0900

    웅희의 match의 웹소켓 구독을 시도 하려 했지만 결제 까지 연동이 안되서 중단함

---
+++ backend/board_api/src/main/java/com/msa/kyj_prj/match/alarm/MatchAlarmMessageDTO.java
@@ -0,0 +1,28 @@
+package com.msa.kyj_prj.match.alarm;
+
+import lombok.Data;
+
+@Data
+public class MatchAlarmMessageDTO implements AlarmMessage {
+    private String type;         // 예: MATCH_RESULT, MATCH_INVITE 등
+    private String senderId;     // 보낸 사람
+    private String receiverId;   // 받는 사람
+    private String message;      // 메시지 내용
+    private String url;          // 알림 클릭 시 이동 경로
+
+    // match만의 추가 필드 (필요시 자유롭게 추가)
+    private Long matchId;
+    private String matchTitle;
+    
+    
+    public MatchAlarmMessageDTO(String type, String senderId, String receiverId, Long matchId, String message, String url) {
+        this.type = type;
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.matchId = matchId;
+        this.message = message;
+        this.url = url;
+    }
+
+    public MatchAlarmMessageDTO() {}
+}
