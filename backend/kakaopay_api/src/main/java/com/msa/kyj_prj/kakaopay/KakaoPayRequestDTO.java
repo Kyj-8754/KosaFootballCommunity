29f31d070ab385ca2135771d5258b4feb2eaff07
Yongjin Kim <span5189@naver.com>
2025-07-08 13:49:51 +0900

    카카오 결제에 권한확인 로직 추가

--- backend/kakaopay_api/src/main/java/com/msa/kyj_prj/kakaopay/KakaoPayRequestDTO.java
+++ backend/kakaopay_api/src/main/java/com/msa/kyj_prj/kakaopay/KakaoPayRequestDTO.java
@@ -21,5 +21,8 @@ public class KakaoPayRequestDTO {
 
     @JsonProperty("total_amount")
     private int totalAmount;
+    
+    @JsonProperty("authCode")
+    private String authCode;
 
 }
--- backend/kakaopay_api/src/main/java/com/msa/kyj_prj/kakaopay/PaymentDTO.java
+++ backend/kakaopay_api/src/main/java/com/msa/kyj_prj/kakaopay/PaymentDTO.java
@@ -1,5 +1,7 @@
 package com.msa.kyj_prj.kakaopay;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PaymentDTO {
 	private Long id;
 	private String tid;
