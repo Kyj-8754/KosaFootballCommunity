8600fe643f54fc42295a9c27925963b79587905d
Sibaru1217 <leehakbin1217@gmail.com>
2025-07-09 15:22:42 +0900

    예약 취소 웹소켓 완성

--- backend/reservation_api/src/main/java/com/msa/kyj_prj/webSocket/ReservationCancelAlarmMessageDTO.java
+++ backend/reservation_api/src/main/java/com/msa/kyj_prj/webSocket/ReservationCancelAlarmMessageDTO.java
@@ -1,4 +1,4 @@
-package com.msa.kyj_prj.match.alarm;
+package com.msa.kyj_prj.webSocket;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,14 +7,13 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class MatchAlarmMessageDTO implements AlarmMessage {
-    private String type;         // 예: MATCH_RESULT, MATCH_INVITE 등
+public class ReservationCancelAlarmMessageDTO implements AlarmMessage {
+	private String type;         // 예: 친구 신청, 승인/거절
     private String senderId;     // 보낸 사람
     private String receiverId;   // 받는 사람
     private String message;      // 메시지 내용
     private String url;          // 알림 클릭 시 이동 경로
 
-    // match만의 추가 필드 (필요시 자유롭게 추가)
-    private Long matchId;
-    private String matchTitle;
+    private Long matchId;   // 연동된 매치 id (nullable)
+    private String matchTitle; // 매치명(알림에서 쓸 수 있음, nullable)
 }
