def9aa347217d9c53d7490af9ae07b085d623ddd
yoo07134 <yoo07134@naver.com>
2025-07-11 10:43:35 +0900

    클럽 매치 리스트 디자인 변경중

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -21,48 +21,7 @@
         <BoardCategoryTabs />
         <RecentMatchList />
         <OldMatchList />
-				<div>
-            <p>현재 시간: {{ serverTime }}</p>
-        </div>
-
-        <div class="table-responsive">
-          <table class="table table-striped table-hover table-bordered mt-4">
-            <thead class="table-dark text-center">
-              <tr>
-                <th>글번호</th>
-                <th>제목</th>
-                <th>작성자</th>
-                <th>등록일</th>
-                <th>조회수</th>
-              </tr>
-            </thead>
-            <tbody class="text-center">
-              <tr 
-                v-if="pageResponse && Array.isArray(pageResponse.list)"  
-                v-for="(item, index) in pageResponse.list" 
-                :key="item.bno">
-                <td>
-                  {{ 
-                    (pageResponse.totalCount || 0) - index 
-                    - ((pageResponse.pageNo || 1) - 1) * (pageResponse.size || 10)
-                  }}
-                </td>
-                <td class="text-truncate" style="max-width: 100%;">
-                  <router-link :to="{ name: 'Board_DetailView', query: { bno: item.bno } }" class="d-inline-block w-100">
-                    {{ item.title }}
-                  </router-link>
-                </td>
-                <td>{{ item.writer }}</td>
-                <td>{{ item.reg_date }}</td>
-                <td>{{ item.view_count }}</td>
-              </tr>
-              <tr v-else>
-                <td colspan="5">게시글이 없습니다.</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-
+        <clubMatchList :clubId="25"/>
       </main>
     </div>
   </div>
@@ -80,6 +39,7 @@ import weatherWidget from '@/components/widget/weatherWidget.vue'
 import BoardCategoryTabs from '@/components/main/BoardCategoryTabs.vue'
 import RecentMatchList from '@/components/main/RecentMatchList.vue'
 import OldMatchList from '@/components/main/OldMatchList.vue'
+import clubMatchList from '@/components/board/club/clubMatchList.vue'
 
 const serverTime = ref('')
 
