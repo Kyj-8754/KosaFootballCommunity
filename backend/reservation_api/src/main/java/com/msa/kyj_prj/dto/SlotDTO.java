c480d3afa27776507859444479ed265ccc2cd219
Kyj-8754 <span5189@naver.com>
2025-07-14 22:19:50 +0900

    예약 API 스웨거 추가중

--- backend/reservation_api/src/main/java/com/msa/kyj_prj/dto/SlotDTO.java
+++ backend/reservation_api/src/main/java/com/msa/kyj_prj/dto/SlotDTO.java
@@ -1,16 +1,21 @@
 package com.msa.kyj_prj.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
+@Schema(description = "게시판 목록 및 페이징 응답 DTO")
 public class SlotDTO {
+	@Schema(description = "예약 슬롯 PK", example = "SVC123")
 	private String slotid;		// 슬롯 pk값
+	@Schema(description = "축구장 서비스ID", example = "SVC123")
 	private String svcid; 		// 서비스 아이디
+	@Schema(description = "이용 날짜", example = "SVC123")
 	private String slotDate;	// 이용 날짜
+	@Schema(description = "이용 시작 시간", example = "SVC123")
 	private String startTime;	// 이용 시작 시간
+	@Schema(description = "이용 종료 시간", example = "SVC123")
 	private String endTime;	// 이용 종료 시간
+	@Schema(description = "예약 상태", example = "SVC123")
 	private String reservationStatus;	// 예약 스테이터스
 }
--- backend/stadium_api/src/main/java/com/msa/kyj_prj/dto/Slot.java
+++ backend/stadium_api/src/main/java/com/msa/kyj_prj/dto/Slot.java
@@ -1,5 +1,6 @@
 package com.msa.kyj_prj.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,9 +9,14 @@ import lombok.NoArgsConstructor;
 @Data @AllArgsConstructor @Builder
 @NoArgsConstructor
 public class Slot {
+	@Schema(description = "예약 슬롯 PK", example = "SVC123")
 	private String SLOT_ID;		// 슬롯 pk값
+	@Schema(description = "구장 서비스ID", example = "SVC123")
 	private String SVCID;		// 서비스 번호
+	@Schema(description = "이용 날짜", example = "SVC123")
 	private String SLOT_DATE;	// 이용 날짜
+	@Schema(description = "이용 시작 시간", example = "SVC123")
 	private String START_TIME;	// 이용 시작 시간
+	@Schema(description = "이용 종료 시간", example = "SVC123")
 	private String END_TIME;	// 이용 종료 시간
 }
