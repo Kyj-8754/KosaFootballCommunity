d9464c642a8664021c6b9f426b95d7439bbd938b
yoo07134 <yoo07134@naver.com>
2025-07-15 15:00:54 +0900

    스웨거 추가

--- backend/weather_api/src/main/java/com/msa/kyj_prj/widget/WidgetController.java
+++ backend/weather_api/src/main/java/com/msa/kyj_prj/widget/WidgetController.java
@@ -1,6 +1,11 @@
 package com.msa.kyj_prj.widget;
 
 import com.msa.kyj_prj.weather.Weather;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,15 +16,17 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/widget")
+@Tag(name = "날씨 위젯", description = "수집한 데이터를 기반으로 단기 예보 데이터를 출력")
 public class WidgetController {
 
     @Autowired
     private WidgetService widgetService;
 
     @GetMapping("/forecast")
+    @Operation(summary = "단기예보 조회",description = "입력받은 지역과 시간 데이터를 기반으로 단기예보를 조회합니다.")
     public ResponseEntity<?> getEarliest5Forecast(
-            @RequestParam("region") String weather_region,
-            @RequestParam("date") String weather_fcst_date
+    		@Parameter(description = "서울 구 명", example = "송파구") @RequestParam("region") String weather_region,
+    		@Parameter(description = "현재시각", example = "YYYYMMDD") @RequestParam("date") String weather_fcst_date
     ) {
         try {
             List<Weather> forecasts = widgetService.getEarliest5TimesForecast(weather_region, weather_fcst_date);
