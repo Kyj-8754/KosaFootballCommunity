93e090dc5ecbeb958ba48b4978e06481f297b54c
Do-seungjin <dosjin01@gmail.com>
2025-07-16 17:54:52 +0900

    [도승진] 컨트롤러 수정

--- backend/loginapi/src/main/java/com/msa/do_login/myPage/controller/MyPageController.java
+++ backend/loginapi/src/main/java/com/msa/do_login/myPage/controller/MyPageController.java
@@ -25,6 +25,8 @@ import com.msa.do_login.user.vo.UserVO;
 import com.msa.do_login.webSocket.WebSocket;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -41,6 +43,10 @@ public class MyPageController {
 	
 	// 회원 상세정보 조회
 	@Operation(summary = "회원 상세정보 조회", description = "회원 번호를 통해 상세 정보를 조회합니다.")
+	@ApiResponses({
+        @ApiResponse(responseCode = "200", description = "회원 정보 조회 성공"),
+        @ApiResponse(responseCode = "404", description = "해당 사용자를 찾을 수 없음")
+    })
 	@GetMapping("/detailView")
 	public ResponseEntity<Map<String, Object>> getMemberDetail(@RequestParam int userNo) {
 	    UserVO userInfo = myPageService.getUserVO(userNo);
@@ -77,6 +83,10 @@ public class MyPageController {
 	
 	// 회원 정보 수정
 	@Operation(summary = "회원 정보 수정", description = "회원 번호를 통해 회원 정보를 수정합니다.")
+	@ApiResponses({
+        @ApiResponse(responseCode = "200", description = "회원 정보 수정 성공"),
+        @ApiResponse(responseCode = "400", description = "회원 정보 수정 실패")
+    })
 	@PostMapping("/update")
 	public ResponseEntity<Map<String, Object>> update(
 			@RequestParam("userNo") int userNo,
@@ -104,6 +114,10 @@ public class MyPageController {
 	
 	// 친구 목록 조회
 	@Operation(summary = "친구 목록 조회", description = "회원 번호를 통해 친구 목록을 조회합니다.")
+	@ApiResponses({
+        @ApiResponse(responseCode = "200", description = "친구 목록 조회 성공"),
+        @ApiResponse(responseCode = "204", description = "조회할 친구가 존재하지 않음")
+    })
 	@GetMapping("/friends")
 	public ResponseEntity<Map<String, Object>> getFriendList(@RequestParam("userNo") int userNo) {
 	    log.info("친구 목록 조회 요청 도착: userNo = {}", userNo);
@@ -126,6 +140,10 @@ public class MyPageController {
 	
 	// 다른 사용자의 친구 목록 조회
 	@Operation(summary = "다른 사용자의 친구 목록 조회", description = "다른 사용자의 친구 목록을 로그인한 사용자의 번호를 통해 조회합니다.")
+	@ApiResponses({
+        @ApiResponse(responseCode = "200", description = "친구 목록 조회 성공"),
+        @ApiResponse(responseCode = "204", description = "조회할 친구가 존재하지 않음")
+    })
 	@GetMapping("/otherFriends")
 	public ResponseEntity<Map<String, Object>> getOtherFriendList(
 	        @RequestParam("targetUserNo") int targetUserNo,
@@ -149,6 +167,10 @@ public class MyPageController {
 	
 	// 친구 검색
 	@Operation(summary = "친구 검색", description = "키워드를 기반으로 친구를 검색합니다.")
+	@ApiResponses({
+        @ApiResponse(responseCode = "200", description = "친구 검색 성공"),
+        @ApiResponse(responseCode = "404", description = "친구 검색 실패")
+    })
 	@GetMapping("/search")
 	public ResponseEntity<Map<String, Object>> searchFriends(@RequestParam("keyword") String keyword,
 			@RequestParam("loginUserNo") int loginUserNo) {
