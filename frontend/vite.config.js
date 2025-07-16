2c6a76054e6fb55eec402ce0197f0a350e23397d
Yongjin Kim <span5189@naver.com>
2025-07-16 15:04:11 +0900

    설정 숨김처리 진행중

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -39,11 +39,6 @@ export default defineConfig({
         target: "http://board_api:8131",
         changeOrigin: true,
       },
-      // 매치 일정 리스트 프록시
-      "/match": {
-        target: "http://board_api:8131", 
-        changeOrigin: true,
-      },
       // 위젯 프록시
       "/widget_api": {
         target: "http://weather_api:8130",
