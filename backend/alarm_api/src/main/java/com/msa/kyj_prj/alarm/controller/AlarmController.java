d8c45858595cc90437f02bbc93429fcd69926344
Do-seungjin <dosjin01@gmail.com>
2025-07-16 10:41:48 +0900

    [도승진] datasource.username 설정 수정

--- backend/alarm_api/src/main/java/com/msa/kyj_prj/alarm/controller/AlarmController.java
+++ backend/alarm_api/src/main/java/com/msa/kyj_prj/alarm/controller/AlarmController.java
@@ -13,7 +13,6 @@ import com.msa.kyj_prj.alarm.dto.AlarmMessageDTO;
 import com.msa.kyj_prj.alarm.service.AlarmHistoryService;
 
 import io.swagger.v3.oas.annotations.Operation;
-
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
